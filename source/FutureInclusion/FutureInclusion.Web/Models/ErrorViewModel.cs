2ed46c94677c30c2b198a7c423e4859bc670e89a
Mario Chapela <mario@chapela.com.br>
2020-04-07 14:29:11 -0300

    Projeto CS criado no VS

---
+++ source/FutureInclusion/FutureInclusion.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FutureInclusion.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
